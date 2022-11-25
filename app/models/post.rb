7b3bf98a41a5a92b15eb167c0c98a3cd40a57706
WMCharles <charles.wafula@student.moringaschool.com>
2022-11-25 10:17:50 +0300

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, :inclusion => { :in => ['Fiction', 'Non-Fiction']}
+    validate :click?
+
+    # Custom validation
+    STRINGS = [/Secret/i, /Won't Believe/i, /Guess/i, /Top \d/i]
+    def click?
+        if STRINGS.none? { |t| t.match title }
+        errors.add(:title, "must be clickbait")
+        end
+    end
+
 end
